eee3a22cfd990e32d6c5592d6318d3d5c81f18f9
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-23 17:05:54 +0100

    Create initial files and folder structure.

---
+++ mobile/App.js
@@ -0,0 +1,8 @@
+// Mobile application entry point
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
