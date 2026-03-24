758788c2c1ca1d7a6f380de510e019d155a5dac9
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-24 02:41:02 +0100

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,13 @@
 //mobile/app.js 
 
 import React from 'react';
-import { View, Text } from 'react-native';
 import { AuthProvider } from './contexts/AuthContext';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
     <AuthProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>AuthProvider actif</Text>
-      </View>
+      <AppNavigator />
     </AuthProvider>
   );
 }
