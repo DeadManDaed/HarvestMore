dc9805c50bd64aaab3ab4f3be7a799b585954f56
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-24 02:31:13 +0100

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -2,11 +2,14 @@
 
 import React from 'react';
 import { View, Text } from 'react-native';
+import { AuthProvider } from './contexts/AuthContext';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Harvest More - Test</Text>
-    </View>
+    <AuthProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>AuthProvider actif</Text>
+      </View>
+    </AuthProvider>
   );
 }
