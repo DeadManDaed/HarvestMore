88598c328392bead4daa9151be3fe297625061c4
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-24 01:17:55 +0100

    Update supabase.jsx

--- mobile-new/lib/supabase.jsx
+++ mobile-new/lib/supabase.jsx
@@ -6,8 +6,8 @@ import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = 'https://qhmgrjeqdsvzetznctyn.supabase.co';   // remplacez par votre URL
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFobWdyamVxZHN2emV0em5jdHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODczMDgsImV4cCI6MjA4OTg2MzMwOH0.uWOViz7bOVoqIZfqmT0LVI-RIBnDXmOr-prn_SuruUo';  
-const 
-/*
+/* const 
+
 EXPO_PUBLIC_SUPABASE_KEY=sb_publishable_s7ri3I6GJ8qsShfAmJYXIg_hD5NntMS
 */       
 
