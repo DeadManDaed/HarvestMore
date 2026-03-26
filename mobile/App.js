53dba3f90bc91d59e8dce9e1f3753cd7d4bb6d51
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-26 21:37:27 +0100

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import AppNavigator from './navigation/AppNavigator';
+import { CartProvider } from './contexts/CartContext';
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppNavigator />
+      <CartProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </CartProvider>
     </AuthProvider>
   );
 }
