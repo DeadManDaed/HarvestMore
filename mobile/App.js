37d58403b0a1b4c6345476330fd7db4819e8abe0
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-28 00:55:51 +0100

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -4,14 +4,19 @@ import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import AppNavigator from './navigation/AppNavigator';
 import { CartProvider } from './contexts/CartContext';
+import { ChatProvider } from './contexts/ChatContext';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-    {/*    <StatusBar style="auto" />    */}
-        <AppNavigator />
-      </CartProvider>
-    </AuthProvider>
+
+// Dans le return :
+<AuthProvider>
+  <CartProvider>
+    <ChatProvider>
+     // <StatusBar style="auto" />
+      <AppNavigator />
+    </ChatProvider>
+  </CartProvider>
+</AuthProvider>
   );
 }
