dfad90b41ac902dd872cbd75c434df7239b540d3
DeadManDaed <160432445+DeadManDaed@users.noreply.github.com>
2026-03-28 02:40:02 +0100

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,22 +1,24 @@
 //mobile/app.js 
 
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './contexts/AuthContext';
-import AppNavigator from './navigation/AppNavigator';
 import { CartProvider } from './contexts/CartContext';
 import { ChatProvider } from './contexts/ChatContext';
+import { NotificationProvider } from './contexts/NotificationContext';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
-
-// Dans le return :
-<AuthProvider>
-  <CartProvider>
-    <ChatProvider>
-     {/* <StatusBar style="auto" />.  */}
-      <AppNavigator />
-    </ChatProvider>
-  </CartProvider>
-</AuthProvider>
+    <AuthProvider>
+      <CartProvider>
+        <ChatProvider>
+          <NotificationProvider>
+         {/*   <StatusBar style="auto" />.  */}
+            <AppNavigator />
+          </NotificationProvider>
+        </ChatProvider>
+      </CartProvider>
+    </AuthProvider>
   );
 }
